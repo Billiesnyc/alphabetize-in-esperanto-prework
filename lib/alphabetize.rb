add98d1a65e2023d1114e309dbfd129c6984d074
Billie Simmons <billiesnyc@gmail.com>
2018-08-24 13:33:37 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,7 @@
 def alphabetize(arr)
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   esp_alphabet_array = esperanto_alphabet.chars
+  esp_hash = { }
   esp_alphabet_array.each_with_index do |letter, index|
     
   arr.sort_by {|phrase| esperanto_alphabet}
