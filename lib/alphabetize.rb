666285cb32215ef7642edde56b577ed88753b05b
Billie Simmons <billiesnyc@gmail.com>
2018-08-24 13:45:20 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,7 @@ def alphabetize(arr)
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   esp_alphabet_array = esperanto_alphabet.chars
   arr.sort_by do |phrase|
-    phrase.chars.each do |letter|
+    phrase.chars.map do |letter|
       esp_alphabet_array.index(letter)
     end
   end
