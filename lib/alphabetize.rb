4ba21cc8aa51d2919bc378c8f693b356105215a8
Billie Simmons <billiesnyc@gmail.com>
2018-08-24 13:14:07 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,4 +1,4 @@
 def alphabetize(arr)
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  array.sort_by {|phrase| esperanto_alphabet.char.index
+  array.sort_by {|phrase| esperanto_alphabet.chars.index}
 end
